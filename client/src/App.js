f0add2a81210fa31e259510f95c1fe4a169100b1
venkatesh cheekatla <109548919+venkatesh-cheekatla@users.noreply.github.com>
2024-06-08 21:07:59 +0530

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,14 @@ const App = () => {
     let vh = window.innerHeight * 0.01;
     document.documentElement.style.setProperty('--vh', `${vh}px`); 
   })
-  
+  axios.default.withCredentials=true;
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    axios.post('https://sudoku-mern-api.vercel.app/register',{email,password})
+    .then(result=>console.log(result))
+    .catch(err=>console.log(err))
+    
+  }
   return (
     <>
         <BrowserRouter>
